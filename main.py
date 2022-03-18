dbeb7532a4b4c2ba71e50301e1126fe7302e5831
Adrian Wójcik <adrian.wojcik00@gmail.com>
2022-03-18 12:30:58 +0100

    Add functions

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
+def read_data():  # Funkcja do odczytu plików .tsv
+    with open('input\Słówka SPEAK UP - Arkusz1.tsv', 'r', encoding='UTF-8') as file:
+        words = file.read().splitlines()
+    convertData = []
+    for data in words[1:]:
+        convertData.append(data.replace('\t', '/').split('/'))
+    return convertData
 
-with open('input\Słówka SPEAK UP - Arkusz1.tsv', 'r', encoding='UTF-8') as file:
-    words = file.read().splitlines()
 
-convertData = []
+def get_words_list():  # Funkcja do wygenerowania listy angielskich słówek
+    wordsList = []
+    for line in read_data():
+        if line[1] != '':
+            wordsList.append(line[1])
+    return wordsList
 
-for data in words[1:]:
-    convertData.append(data.replace('\t', '/').split('/'))
 
-wordsList = []
-for line in convertData:
-    if line[1] != '':
-        wordsList.append(line[1])
+def check_white_space():  # Funkcja do sprawadzania białych zanków w pliku bazowym
+    for i in read_data():
+        if i[1].endswith(' ') or i[1].startswith(' '):
+            print(f'{i[0]}  --{i[1]}--')
 
-print(wordsList)
+
+# check_white_space()
+#print(f'Dane po odczycie z pliku: {read_data()}')
+print(f'Lista słowek dla Chrupka: {get_words_list()}')
